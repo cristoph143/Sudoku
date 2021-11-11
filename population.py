ed8130a7e942ecfe7f54fb2f7b47a1bcbed2ee44
Cristopher Bohol <cristopher.bohol.21@usjr.edu.ph>
2021-11-11 16:03:38 +0800

    Setting Tournament Selection
    Fixed Candidate Values

--- population.py
+++ population.py
@@ -24,39 +24,36 @@ class Population(object):
                     if given[row][column] != 0:
                         # Given/known value from file.
                         helper.values[row][column] = given[row][column]
-                    else:
-                        if given[row][column] == 0 and not (self.is_column_duplicate(column, value) or self.is_block_duplicate(row, column, value) or self.is_row_duplicate(row, value)):
+                        break
+                    elif given[row][column] == 0 and not (self.is_column_duplicate(column, value) or self.is_block_duplicate(row, column, value) or self.is_row_duplicate(row, value)):
                             helper.values[row][column].append(value)
         
         # Seed a new population.       
         for p in range(0, Nc):
             g = Candidate()
+
             for i in range(0, Nd): # New row in candidate.
                 row = numpy.zeros(Nd,dtype=int)
-                
                 # Fill in the givens.
                 for j in range(0, Nd): # New column j value in row i.
-                
                     # If value is already given, don't change it.
                     if(given[i][j] != 0):
                         row[j] = given[i][j]
                     # Fill in the gaps using the helper board.
                     elif(given[i][j] == 0):
                         row[j] = helper.values[i][j][random.randint(0, len(helper.values[i][j])-1)]
-
                 # If we don't have a valid board, then try again. There must be no duplicates in the row.
                 while(len(list(set(row))) != Nd):
                     for j in range(0, Nd):
                         if(given[i][j] == 0):
                             row[j] = helper.values[i][j][random.randint(0, len(helper.values[i][j])-1)]
-
                 g.values[i] = row
             self.candidates.append(g)
-        print("Seeding complete.")
+            
         # Compute the fitness of all candidates in the population.
-        print('Start\n--------------------------------')
+        print('-------------Start------------------')
         self.update_fitness()
-        print('End\n----------------------------------')
+        print('--------------End--------------------')
         print("Seeding complete.")
         return
 
@@ -104,7 +101,7 @@ class Population(object):
         return
 
     def sort(self):
-        print(self.candidates.sort(key=cmp_to_key(self.sort_fitness)))
+        self.candidates.sort(key=cmp_to_key(self.sort_fitness))
         return
 
     def sort_fitness(self, x, y):
--- sudoku_game.py
+++ sudoku_game.py
@@ -2,6 +2,7 @@
 import numpy as np
 from candidate import Candidate
 from population import Population
+from tournament import Tournament
 
 
 class Sudoku:
@@ -11,8 +12,8 @@ class Sudoku:
         return
 
     def solve(self, sudoku_given):
-        Nc = 5  # Number of candidates (i.e. population size).
-        Ne = int(0.05*Nc)  # Number of elites.
+        Nc = 10  # Number of candidates (i.e. population size).
+        Ne = int(0.1*Nc)  # Number of elites.
         Ng = 10  # Number of generations.
 
         # Create an initial population.
@@ -21,6 +22,7 @@ class Sudoku:
 
         # For up to 10000 generations...
         stale = 0
+        print('------------Generation---------------')
         for generation in range(0, Ng):
         
             print("Generation %d" % generation)
@@ -39,14 +41,27 @@ class Sudoku:
                     best_fitness = fitness
 
             print("Best fitness: %f" % best_fitness)
+        print('-----------End Generation-------------')
             
-        # Create the next population.
-        next_population = []
-
         # Select elites (the fittest candidates) and preserve them for the next generation.
         self.population.sort()
+
+        print('---------------Elites-----------------')
+        print(Ne)
         elites = []
         for e in range(0, Ne):
             elite = Candidate()
             elite.values = np.copy(self.population.candidates[e].values)
             elites.append(elite)
+            # print(f'Elite {e}: {elites[e]}')
+        print('-------------End Elites---------------')
+
+        # Create the rest of the candidates.
+        print('-------------Tournament---------------')
+        for count in range(Ne, Nc, 2):
+        # Select parents from population via a tournament.
+            t = Tournament()
+            parent1 = t.compete(self.population.candidates)
+            parent2 = t.compete(self.population.candidates)
+            print(f'Parent 1:\n{parent1.values}\nParent 2:\n {parent2.values}')
+        print('-----------End Tournament-------------')
